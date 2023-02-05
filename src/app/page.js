7f1e946585eddbfc8e2d50ee5b60a8f9d2aa1fd6
Denis Djordjevic <denis@MacBook-Air.local>
2023-02-05 23:21:17 +0100

    fixed window issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,10 @@
-'use client'
-
-import useColorMode from '@/hooks/useColorMode'
+import Switch from '@/components/switch'
 
 export default function Home() {
-  const [colorMode, setColorMode] = useColorMode()
-
   return (
     <main className='h-screen w-screen p-6 bg-white dark:bg-black'>
-      <button
-        className='dark:text-white text-black bg-sky-500 dark:bg-green-500'
-        onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-      >
-        TOGGLE THEME
-      </button>
+      <Switch />
+      <h1 className='text-red-600 dark:text-gray-200'>test teme</h1>
     </main>
   )
 }
--- src/components/switch.jsx
+++ src/components/switch.jsx
@@ -1,9 +1,16 @@
+'use client'
+
 import useColorMode from '@/hooks/useColorMode'
 
 export default function Switch() {
   const [colorMode, setColorMode] = useColorMode()
 
   return (
-    
+    <button
+      className='dark:text-white text-black bg-sky-500 dark:bg-green-500'
+      onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+    >
+      TOGGLE THEME
+    </button>
   )
 }
